d8982c7eac118f27e3128f9b2e742c6e93ed3942
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-02-06 18:27:12 +0000

    setting up grid

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -1,5 +1,29 @@
-describe('A player', funciton(){
-  it('', function(){
-    
-  })
-})
+describe('Player', function(){
+  it('has an empty playerGrid at start', function(){
+    player = new Player();
+    expect(player.playerGrid).toEqual([]);
+  });
+  it('has an empty enemyGrid at start', function(){
+    player = new Player();
+    expect(player.enemyGrid).toEqual([]);
+  });
+
+  it('can initialize a grid at the start of the game', function(){
+    player = new Player();
+    console.log(initializeGrid(player));
+    var zeroMemberMatrix = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]
+    ]
+    expect(player.initializeGrid).toEqual(zeroMemberMatrix);
+  });
+
+});
---
+++ src/player.js
@@ -0,0 +1,34 @@
+function Player () {
+this.playerGrid = [];
+this.enemyGrid = [];
+
+}
+
+var initializeGrid = function(player, rowNum, colNum){
+  for(var i = 00000; i < rowNum; i++){
+
+  }
+
+  return zeroGrid;
+};
+
+var shipTypes = {
+  1: 4,
+  2: 3,
+  3: 2,
+  4: 1
+};
+
+Player.prototype.setupShips = function () {
+
+};
+
+
+
+Player.prototype.attack = function (row, col) {
+
+};
+
+Player.prototype.receiveDemage = function (row, col) {
+
+};
