ba4c55175ae1ca2c24ed58b84b567f75414d2218
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-02-06 18:57:36 +0000

    setting up ships on grid

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -1,16 +1,17 @@
 describe('Player', function(){
-  it('has an empty playerGrid at start', function(){
+  var player;
+  beforeEach(function(){
     player = new Player();
+  })
+
+  it('has an empty playerGrid at start', function(){
     expect(player.playerGrid).toEqual([]);
   });
   it('has an empty enemyGrid at start', function(){
-    player = new Player();
     expect(player.enemyGrid).toEqual([]);
   });
 
-  it('can initialize a grid at the start of the game', function(){
-    player = new Player();
-    console.log(initializeGrid(player));
+  it('can initialize both grids at the start of the game', function(){
     var zeroMemberMatrix = [
       [0,0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0,0],
@@ -23,7 +24,15 @@ describe('Player', function(){
       [0,0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0,0]
     ]
-    expect(player.initializeGrid).toEqual(zeroMemberMatrix);
+    expect(initializeGrid(player, 10, 10)).toEqual(zeroMemberMatrix);
+  });
+
+  it('can place 4 submarines (1 space) randomly', function (){
+    var shipsToSetup = {4:1};
+    player.setupShips(shipsToSetup);
+    expect(player.playerGrid.reduce(function(a, b){
+      return a+b
+    }, 0)).toEqual(4);
   });
 
 });
--- src/player.js
+++ src/player.js
@@ -4,25 +4,31 @@ this.enemyGrid = [];
 
 }
 
+var zeroGrid = [];
+var zeroRows = [];
 var initializeGrid = function(player, rowNum, colNum){
-  for(var i = 00000; i < rowNum; i++){
-
-  }
-
+  for(var i = 0; i < rowNum; i++){
+    for(var j = 0; j < colNum; j++){
+      zeroRows.push(0);
+    };
+    zeroGrid.push(zeroRows);
+    zeroRows = [];
+  };
+  this.playerGrid = zeroGrid;
+  this.enemyGrid = zeroGrid;
   return zeroGrid;
 };
 
-var shipTypes = {
-  1: 4,
-  2: 3,
-  3: 2,
-  4: 1
+// var shipTypes = {
+//   1: 4,
+//   2: 3,
+//   3: 2,
+//   4: 1
+// };
+
+Player.prototype.setupShips = function (shipsToSetup) {
+  
 };
-
-Player.prototype.setupShips = function () {
-
-};
-
 
 
 Player.prototype.attack = function (row, col) {
